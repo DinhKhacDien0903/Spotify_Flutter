d9c7f692eba96f8a5fa495a1bbbcf7ffb7b03d03
diendk <dinhkhacdien1009@gmail.com>
2025-08-14 17:33:06 +0700

    update code

--- spotify_app/lib/presentation/home/pages/home.dart
+++ spotify_app/lib/presentation/home/pages/home.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:spotify_app/common/helpers/is_dark_mode.dart';
 import 'package:spotify_app/common/widgets/appbar/app_bar.dart';
 import 'package:spotify_app/core/configs/assets/app_images.dart';
 import 'package:spotify_app/core/configs/assets/app_vectors.dart';
+import 'package:spotify_app/core/configs/theme/app_colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +32,9 @@ class HomePage extends StatelessWidget {
         iconSize: 30,
         onLeadingPressed: () {},
       ),
-      body: SingleChildScrollView(child: Column(children: [_homeArtistCard()])),
+      body: SingleChildScrollView(
+        child: Column(children: [_homeArtistCard(), _tabbar()]),
+      ),
     );
   }
 
@@ -40,6 +58,17 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _tabbar() {
+    return TabBar(
+      padding: EdgeInsets.only(top: 40),
+      isScrollable: true,
+      labelColor: context.isDarkMode ? Colors.white : Colors.black,
+      indicatorColor: AppColors.primary,
+      controller: _tabController,
+      tabs: [Text('News'), Text('Videos'), Text('Artists'), Text('Podcasts')],
     );
   }
 }
